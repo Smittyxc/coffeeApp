94377414e3f73b74210223fdf6a3221b6e52f66c
Matt Smith <mattsmith1652@gmail.com>
2025-09-02 13:40:19 -0400

    radar chart example added

--- src/components/radarDemo.tsx
+++ src/components/radarDemo.tsx
@@ -1,25 +1,27 @@
+import { useEffect, useState } from 'react';
 import { Radar, RadarChart, PolarGrid, PolarAngleAxis, PolarRadiusAxis, ResponsiveContainer, Treemap } from 'recharts';
+import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from './ui/select';
 
 const data = [
   {
     subject: 'Acidity',
-    A: 10,
+    score: 5,
   },
   {
     subject: 'Body',
-    A: 4,
+    score: 0,
   },
   {
-    subject: 'Sweet',
-    A: 6,
+    subject: 'Sweetness',
+    score: 0,
   },
   {
-    subject: 'Bitter',
-    A: 1,
+    subject: 'Bitterness',
+    score: 0,
   },
   {
     subject: 'Clarity',
-    A: 8,
+    score: 0,
   },
 ];
 
@@ -39,18 +41,56 @@ const treeMapData = [
   },
 ];
 
-
 const RadarDemo = () => {
- 
+  const [rating, setRating] = useState(data)
+  const qualities = ['Acidity', 'Sweetness', 'Body', 'Bitterness', 'Clarity']
+
+  
+  const handleRatingChange = (score: string, quality: string) => {
+    setRating(prevRatings => 
+      prevRatings.map(item =>
+        item.subject === quality
+        ? {...item, score: Number(score)}
+        : item
+      )
+    )
+  }
+  useEffect(() =>
+    console.log(rating),
+    [rating]
+  )
+
+  
   return (
     <div className='min-h-screen w-full'>
-      <div className='h-100 w-full flex justify-center'>
+      <div className='h-100 w-full grid grid-rows-2 justify-center items-center'>
+        <div className='flex flex-wrap gap-2 items-center'>
+          {qualities.map(quality => {
+            return (
+              <Select onValueChange={value => handleRatingChange(value, quality)}>
+                <SelectTrigger className="w-fit">
+                  <SelectValue placeholder={quality} />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup>
+                    <SelectLabel>{quality}</SelectLabel>
+                    <SelectItem value="1">1</SelectItem>
+                    <SelectItem value="2">2</SelectItem>
+                    <SelectItem value="3">3</SelectItem>
+                    <SelectItem value="4">4</SelectItem>
+                    <SelectItem value="5">5</SelectItem>
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
+            )
+          })}
+        </div>
         <ResponsiveContainer width="90%" height="100%">
-          <RadarChart cx="50%" cy="50%" outerRadius="75%" data={data} >
+          <RadarChart outerRadius="75%" data={rating} >
             <PolarGrid />
-            <PolarAngleAxis dataKey="subject" tick={{fill: '#171717'}} />
-            <PolarRadiusAxis />
-            <Radar name="Coffee" dataKey="A" stroke="#8884d8" fill="#8884d8" fillOpacity={0.6} />
+            <PolarAngleAxis dataKey="subject" domain={[0,5]} tick={{fill: '#171717'}} />
+            <PolarRadiusAxis domain={[0,6]} />
+            <Radar name="Coffee" dataKey="score" stroke="#8884d8" fill="#8884d8" fillOpacity={0.6} />
           </RadarChart>
         </ResponsiveContainer>
       </div>
